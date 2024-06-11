da935730456dbfad868edd163b3c504bb5f50ee6
tank-justmeat <tank@justmeats.com>
2024-06-11 14:12:31 +0530

    redirect uri change

--- admin1/googleconfig.php
+++ admin1/googleconfig.php
@@ -4,7 +4,7 @@ require_once '../vendor/autoload.php';
 // init configuration
 $clientID = '341622769724-9id4hviqggn6lukh1lidaftnq4sk41uu.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'http://' . $_SERVER['HTTP_HOST'] . '/markets/admin1/index.php';
+$redirectUri = SITE_ADMIN_URL.'index.php';
 // https://codelocksolutions.in/YouTube/php-google-login/welcome.php
 
 // create Client Request to access Google API
