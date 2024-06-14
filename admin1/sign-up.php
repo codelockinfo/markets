d22bd280c5a92988168523689f9d464498c8039a
codelockinfo <codelockinfo>
2024-06-14 12:39:00 +0530

    remove comment

--- admin1/sign-up.php
+++ admin1/sign-up.php
@@ -147,15 +147,9 @@ include 'header.php';
     <div class="container">
       <div class="row">
         <div class="col-lg-8 mb-4 mx-auto text-center">
-          <!-- <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Company
-            </a> -->
           <a href="#" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
             About Us
           </a>
-          <!-- <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
-              Team
-            </a> -->
           <a href="<?php echo CLS_SITE_URL ?>product.php" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
             Products
           </a>
