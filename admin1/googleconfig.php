990ace6d83364162b2e204d09ea6b760b297f372
tank-justmeat <tank@justmeats.com>
2024-06-17 11:06:56 +0530

    clientID & clientSecret key  define blank

--- admin1/googleconfig.php
+++ admin1/googleconfig.php
@@ -5,6 +5,8 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
   $thirdparty_apidata = mysqli_fetch_assoc($result);
 }
+$clientID = "";
+$clientSecret = "";
 if(!empty($thirdparty_apidata)){
     $clientID = $thirdparty_apidata['clientID'];
     $clientSecret = $thirdparty_apidata['clientSecret'];
