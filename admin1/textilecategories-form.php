9ce53af7d2bede0c0d5f270c9fb14b1a3f7ad9db
codelockinfo <codelockinfo>
2024-06-25 18:18:51 +0530

    all page changes

--- admin1/textilecategories-form.php
+++ admin1/textilecategories-form.php
@@ -68,57 +68,59 @@ include 'header.php';
                 <button type="button" class="btn bg-gradient-info btn-sm">Enable</button>
                 <button type="button" class="btn bg-gradient-info btn-sm">Disable</button>
               </div>
-              <div class="col-xl-4 col-md-6 mb-xl-0 mb-2">
+              <div class="col-xl-6 col-md-6 mb-xl-0 mb-2">
                 <div class="card card-blog card-plain">
                   <div class="position-relative">
                     <a class="d-block border-radius-xl">
-                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-6 mt-3">
+                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-3 mt-3">
                     </a>
+                  </div>
+                  <div class="d-flex justify-content-between mb-3">
+                    <div class="ms-auto text-end">
+                      <button type="button" class="btn btn-outline-danger text-danger px-3 btn-sm pt-2 mb-0">Delete</button>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div class="col-xl-4 col-md-6 mb-xl-0 mb-2">
+              <div class="col-xl-6 col-md-6 mb-xl-0 mb-2">
                 <div class="card card-blog card-plain">
                   <div class="position-relative">
                     <a class="d-block border-radius-xl">
-                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-6 mt-3">
+                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-3 mt-3">
                     </a>
+                  </div>
+                  <div class="d-flex justify-content-between mb-3">
+                    <div class="ms-auto text-end">
+                      <button type="button" class="btn btn-outline-danger text-danger px-3 btn-sm pt-2 mb-0">Delete</button>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div class="col-xl-4 col-md-6 mb-xl-0 mb-2">
+              <div class="col-xl-6 col-md-6 mb-xl-0 mb-2">
                 <div class="card card-blog card-plain">
                   <div class="position-relative">
                     <a class="d-block border-radius-xl">
-                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-6 mt-3">
+                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-3 mt-3">
                     </a>
+                  </div>
+                  <div class="d-flex justify-content-between mb-3">
+                    <div class="ms-auto text-end">
+                      <button type="button" class="btn btn-outline-danger text-danger px-3 btn-sm pt-2 mb-0">Delete</button>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div class="col-xl-4 col-md-6 mb-xl-0 mb-2">
+              <div class="col-xl-6 col-md-6 mb-xl-0 mb-2">
                 <div class="card card-blog card-plain">
                   <div class="position-relative">
                     <a class="d-block border-radius-xl">
-                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-6 mt-3">
+                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-3 mt-3">
                     </a>
                   </div>
-                </div>
-              </div>
-              <div class="col-xl-4 col-md-6 mb-xl-0 mb-2">
-                <div class="card card-blog card-plain">
-                  <div class="position-relative">
-                    <a class="d-block border-radius-xl">
-                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-6 mt-3">
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-xl-4 col-md-6 mb-xl-0 mb-2">
-                <div class="card card-blog card-plain">
-                  <div class="position-relative">
-                    <a class="d-block border-radius-xl">
-                      <img src="<?php echo main_url('/admin1/assets/img/kurti/msg-1001446435108-2955.jpg'); ?>" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg mb-6 mt-3">
-                    </a>
+                  <div class="d-flex justify-content-between mb-3">
+                    <div class="ms-auto text-end">
+                      <button type="button" class="btn btn-outline-danger text-danger px-3 btn-sm pt-2 mb-0">Delete</button>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -128,71 +130,67 @@ include 'header.php';
       </div>
     </div>
     <div class="fixed-plugin">
-      <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-        <i class="fa fa-cog py-2"> </i>
-      </a>
-      <div class="card shadow-lg ">
-        <div class="card-header pb-0 pt-3 ">
-          <div class="float-start">
-            <h5 class="mt-3 mb-0">Soft UI Configurator</h5>
-            <p>See our dashboard options.</p>
-          </div>
-          <div class="float-end mt-4">
-            <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-              <i class="fa fa-close"></i>
-            </button>
-          </div>
-          <!-- End Toggle Button -->
+    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
+      <i class="fa fa-phone"></i>
+    </a>
+    <div class="card shadow-lg ">
+      <div class="card-header pb-0 pt-3 ">
+        <div class="float-start">
+          <h5 class="mt-3 mb-0">Soft UI Configurator</h5>
+          <p>See our dashboard options.</p>
         </div>
-        <hr class="horizontal dark my-1">
-        <div class="card-body pt-sm-3 pt-0">
-          <!-- Sidebar Backgrounds -->
-          <div>
-            <h6 class="mb-0">Sidebar Colors</h6>
+        <div class="float-end mt-4">
+          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+            <i class="fa fa-close"></i>
+          </button>
+        </div>
+      </div>
+      <hr class="horizontal dark my-1">
+      <div class="card-body pt-sm-3 pt-0">
+        <div>
+          <h6 class="mb-0">Sidebar Colors</h6>
+        </div>
+        <a href="javascript:void(0)" class="switch-trigger background-color">
+          <div class="badge-colors my-2 text-start">
+            <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
+            <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
           </div>
-          <a href="javascript:void(0)" class="switch-trigger background-color">
-            <div class="badge-colors my-2 text-start">
-              <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-              <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-              <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-              <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-              <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-              <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-            </div>
+        </a>
+        <div class="mt-3">
+          <h6 class="mb-0">Sidenav Type</h6>
+          <p class="text-sm">Choose between 2 different sidenav types.</p>
+        </div>
+        <div class="d-flex">
+          <button class="btn bg-gradient-primary w-100 px-3 mb-2 active" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
+          <button class="btn bg-gradient-primary w-100 px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
+        </div>
+        <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
+        <div class="mt-3">
+          <h6 class="mb-0">Navbar Fixed</h6>
+        </div>
+        <div class="form-check form-switch ps-0">
+          <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
+        </div>
+        <hr class="horizontal dark my-sm-4">
+        <a class="btn bg-gradient-dark w-100" href="#">Free Download</a>
+        <a class="btn btn-outline-dark w-100" href="#">View documentation</a>
+        <div class="w-100 text-center">
+          <a class="github-button" href="#" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/soft-ui-dashboard on GitHub">Star</a>
+          <h6 class="mt-3">Thank you for sharing!</h6>
+          <a href="#" class="btn btn-dark mb-0 me-2" target="_blank">
+            <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
           </a>
-          <!-- Sidenav Type -->
-          <div class="mt-3">
-            <h6 class="mb-0">Sidenav Type</h6>
-            <p class="text-sm">Choose between 2 different sidenav types.</p>
-          </div>
-          <div class="d-flex">
-            <button class="btn bg-gradient-primary w-100 px-3 mb-2 active" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-            <button class="btn bg-gradient-primary w-100 px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-          </div>
-          <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
-          <!-- Navbar Fixed -->
-          <div class="mt-3">
-            <h6 class="mb-0">Navbar Fixed</h6>
-          </div>
-          <div class="form-check form-switch ps-0">
-            <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-          </div>
-          <hr class="horizontal dark my-sm-4">
-          <a class="btn bg-gradient-dark w-100" href="#">Free Download</a>
-          <a class="btn btn-outline-dark w-100" href="#">View documentation</a>
-          <div class="w-100 text-center">
-            <a class="github-button" href="#" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/soft-ui-dashboard on GitHub">Star</a>
-            <h6 class="mt-3">Thank you for sharing!</h6>
-            <a href="#" class="btn btn-dark mb-0 me-2" target="_blank">
-              <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-            </a>
-            <a href="#" class="btn btn-dark mb-0 me-2" target="_blank">
-              <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-            </a>
-          </div>
+          <a href="#" class="btn btn-dark mb-0 me-2" target="_blank">
+            <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
+          </a>
         </div>
       </div>
     </div>
+  </div>
     <script src="<?php echo main_url('/admin1/assets/js/common.js'); ?>"></script>
 </body>
 
