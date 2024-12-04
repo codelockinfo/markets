95d9aeff2429038438d94b03196370479a9da75a
urvashi <urvashi.codelock99@gmail.com>
2024-12-04 11:10:56 +0530

    change

--- admin1/functions.php
+++ admin1/functions.php
@@ -2480,8 +2480,8 @@ $NO_IMAGE =  "../admin1/assets/img/image_not_found.png";
     function userlisting() {
         $response_data = array('data' => 'fail', 'msg' => "Error");
         global $NO_IMAGE;
-        echo "HEREREE";die;
         if (isset($_SESSION['current_user']['user_id'])) {
+            echo "HEREREE STEP11";die;
             $search_value = isset($_POST['search_text']) ? $this->db->real_escape_string(trim($_POST['search_text'])) : '';
             $limit = 12;
             $page = isset($_POST['page']) ? (int)$_POST['page'] : 1;
