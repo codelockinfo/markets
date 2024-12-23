ee98cc307557f1fb30d595f96461149514fc9a74
urvashi <urvashi.codelock99@gmail.com>
2024-12-23 10:41:20 +0530

    Register mail changes & remove image id change in invoice file

--- admin1/thankemail_template.php
+++ admin1/thankemail_template.php
@@ -8,21 +8,13 @@
     <div class="main-sec" style="max-width: 1140px;margin: auto;background-color: #ffff;border-radius: 30px;">
         <div class="mail-img"
             style="background-color: #fff;margin: auto;padding: 50px 100px;border-radius: 30px 30px 0px 0px;">
-            <img src="https://codelocksolutions.in/markets/admin1/assets/img/welcomebanner.jpg" alt=""
+            <img src="https://codelocksolutions.in/markets/admin1/assets/img/thankyou.jpg" alt=""
                 style="width: 75%;margin: 0 auto;display: flex;">
-            <div class="details" style="text-align: center;">
-                <h3 style="font-size: 38px;margin-bottom: 15px;color: rgb(25, 25, 25);">Welcome to Market Search!</h3>
-                <p style="font-size: 25px;">Thank you for registering on our portal!</p>
-            </div>
+           
+            <img src="https://codelocksolutions.in/markets/admin1/assets/img/welcome.jpg" alt=""
+                style="width: 75%;margin: 0 auto;display: flex;">
+           
         </div>
-    </div>
-    <div class="main-sec" style="max-width: 1140px;margin: auto;background-color: #ffff;border-radius: 30px;">
-        <footer>
-            <div class="footer-text"
-                style="max-width: 650px;text-align: center;margin: 0 auto;color: rgb(85, 85, 85);padding-bottom: 15px;">
-                <p style="margin-bottom: 20px;padding: 0 10px;">If you have any questions or need assistance, please feel free to contact us at codelockinfo@gmail.com.</p>
-            </div>
-        </footer>
     </div>
 </body>
 </html>
