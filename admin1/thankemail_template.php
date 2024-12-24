34ac62e61217f0b277b23e771ec7d03c4530344d
urvashi <urvashi.codelock99@gmail.com>
2024-12-24 17:46:18 +0530

    email image setup dynam

--- admin1/thankemail_template.php
+++ admin1/thankemail_template.php
@@ -1,3 +1,11 @@
+<?php
+$myRoot = $_SERVER["DOCUMENT_ROOT"];
+if ($_SERVER['SERVER_NAME'] == 'textilemarkethub.com') {
+  include($myRoot .'/connection.php');
+}else{
+  include($myRoot . '/markets/connection.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +16,15 @@
     <div class="main-sec" style="max-width: 1140px;margin: auto;background-color: #ffff;border-radius: 30px;">
         <div class="mail-img"
             style="background-color: #fff;margin: auto;padding: 50px 100px;border-radius: 30px 30px 0px 0px;">
-           
-            <img src="https://codelocksolutions.in/markets/admin1/assets/img/thankyou.jpg" alt=""
+            <img src="<?php echo SITE_ADMIN_URL; ?>/assets/img/thankyou.jpg" alt=""
+                style="width: 85%;margin: 0 auto;display: flex;">
+            <img src="<?php echo SITE_ADMIN_URL; ?>/assets/img/welcome.jpg" alt=""
+                style="width: 85%;margin: 0 auto;display: flex;">
+            <!-- <img src="https://codelocksolutions.in/markets/admin1/assets/img/thankyou.jpg" alt=""
                 style="width: 85%;margin: 0 auto;display: flex;">
            
             <img src="https://codelocksolutions.in/markets/admin1/assets/img/welcome.jpg" alt=""
-                style="width: 85%;margin: 0 auto;display: flex;">
+                style="width: 85%;margin: 0 auto;display: flex;"> -->
            
         </div>
     </div>
