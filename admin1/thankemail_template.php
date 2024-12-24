1e9ae332056cd90931cff74c5d423614aae96391
urvashi <urvashi.codelock99@gmail.com>
2024-12-24 18:05:42 +0530

    email image setup dynamic

--- admin1/thankemail_template.php
+++ admin1/thankemail_template.php
@@ -1,10 +1,12 @@
 <?php
 $myRoot = $_SERVER["DOCUMENT_ROOT"];
 if ($_SERVER['SERVER_NAME'] == 'textilemarkethub.com') {
-  include($myRoot .'/connection.php');
+    include($myRoot .'/connection.php');
 }else{
-  include($myRoot . '/markets/connection.php');
+    include($myRoot . '/markets/connection.php');
 }
+$thankyou = SITE_ADMIN_URL .'assets/img/thankyou.jpg';
+$welcome = SITE_ADMIN_URL .'assets/img/welcome.jpg';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,9 +18,9 @@ if ($_SERVER['SERVER_NAME'] == 'textilemarkethub.com') {
     <div class="main-sec" style="max-width: 1140px;margin: auto;background-color: #ffff;border-radius: 30px;">
         <div class="mail-img"
             style="background-color: #fff;margin: auto;padding: 50px 100px;border-radius: 30px 30px 0px 0px;">
-            <img src="<?php echo SITE_ADMIN_URL; ?>assets/img/thankyou.jpg" alt=""
+            <img src="<?php echo $thankyou ?>" alt=""
                 style="width: 85%;margin: 0 auto;display: flex;">
-            <img src="<?php echo SITE_ADMIN_URL; ?>assets/img/welcome.jpg" alt=""
+            <img src="<?php echo $welcome ?>" alt=""
                 style="width: 85%;margin: 0 auto;display: flex;">
             <!-- <img src="https://codelocksolutions.in/markets/admin1/assets/img/thankyou.jpg" alt=""
                 style="width: 85%;margin: 0 auto;display: flex;">
