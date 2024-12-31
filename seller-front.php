dfb9788788c438b923807f8e9c6fc2b60d979058
codelockinfo <codelockinfo>
2024-12-31 16:53:53 +0530

    unused class remove

--- seller-front.php
+++ seller-front.php
@@ -15,16 +15,16 @@
     <!-- categori slider -->
     <section class="light-color py-60 category-section">
         <div class="custom-container">
-            <h1 class="fw-bold main-font-familly mb-5">Categories You May Like</h1>
+            <h1 class="fw-bold heading-font mb-5">Categories You May Like</h1>
             <div class="swiper mySwiper">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide border-r">
                         <div class="card">
                             <img src="img/img6.jpg" />
                             <div class="d-flex justify-content-between align-items-center p-2">
-                                <h6 class="fw-bold f-18 body-font-familly m-0">Kurti</h6>
+                                <h6 class="fw-bold f-18  m-0">Kurti</h6>
                                 <a href="#"
-                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0 body-font-familly mt-2 mb-2 d-flex">View
+                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0  mt-2 mb-2 d-flex">View
                                     all</a>
                             </div>
                         </div>
@@ -33,9 +33,9 @@
                         <div class="card">
                             <img src="img/img7.jpg" />
                             <div class="p-2 d-flex justify-content-between align-items-center">
-                                <h6 class="fw-bold f-18 body-font-familly m-0">Gown</h6>
+                                <h6 class="fw-bold f-18  m-0">Gown</h6>
                                 <a href="#"
-                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0 body-font-familly mt-2 mb-2 d-flex">View
+                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0  mt-2 mb-2 d-flex">View
                                     all</a>
                             </div>
                         </div>
@@ -44,11 +44,11 @@
                         <div class="card">
                             <img src="img/img11.jpg" />
                             <div class="p-2 d-flex justify-content-between align-items-center">
-                                <h6 class="fw-bold f-18 body-font-familly m-0">
+                                <h6 class="fw-bold f-18  m-0">
                                     Traditional
                                 </h6>
                                 <a href="#"
-                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0 body-font-familly mt-2 mb-2 d-flex">View
+                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0  mt-2 mb-2 d-flex">View
                                     all</a>
                             </div>
                         </div>
@@ -57,11 +57,11 @@
                         <div class="card">
                             <img src="img/img5.jpg" />
                             <div class="p-2 d-flex justify-content-between align-items-center">
-                                <h6 class="fw-bold f-18 body-font-familly m-0">
+                                <h6 class="fw-bold f-18  m-0">
                                     Bride Choli
                                 </h6>
                                 <a href="#"
-                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0 body-font-familly mt-2 mb-2 d-flex">View
+                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0  mt-2 mb-2 d-flex">View
                                     all</a>
                             </div>
                         </div>
@@ -70,9 +70,9 @@
                         <div class="card">
                             <img src="img/img12.jpg" />
                             <div class="p-2 d-flex justify-content-between align-items-center">
-                                <h6 class="fw-bold f-18 body-font-familly m-0">tunic</h6>
+                                <h6 class="fw-bold f-18  m-0">tunic</h6>
                                 <a href=""
-                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0 body-font-familly mt-2 mb-2 d-flex">View
+                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0  mt-2 mb-2 d-flex">View
                                     all</a>
                             </div>
                         </div>
@@ -81,9 +81,9 @@
                         <div class="card">
                             <img src="img/img13.jpg" />
                             <div class="p-2 d-flex justify-content-between align-items-center">
-                                <h6 class="fw-bold f-18 body-font-familly m-0">Shervani</h6>
+                                <h6 class="fw-bold f-18  m-0">Shervani</h6>
                                 <a href="#"
-                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0 body-font-familly mt-2 mb-2 d-flex">View
+                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0  mt-2 mb-2 d-flex">View
                                     all</a>
                             </div>
                         </div>
@@ -92,9 +92,9 @@
                         <div class="card">
                             <img src="img/img15.jpg" />
                             <div class="p-2 d-flex justify-content-between align-items-center">
-                                <h6 class="fw-bold f-18 body-font-familly m-0">crop Top</h6>
+                                <h6 class="fw-bold f-18  m-0">crop Top</h6>
                                 <a href="#"
-                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0 body-font-familly mt-2 mb-2 d-flex">View
+                                    class="text-decoration-none custom_button z-index main-color border-r px-4 py-1 fw-semibold border-0  mt-2 mb-2 d-flex">View
                                     all</a>
                             </div>
                         </div>
@@ -108,7 +108,7 @@
     <section class="light-color py-60">
         <div class="custom-container">
             <div class="d-flex justify-content-between align-items-center mb-3 mb-md-5">
-                <h2 class="fw-bold main-font-familly f-36">Trending Video</h2>
+                <h2 class="fw-bold heading-font f-36">Trending Video</h2>
                 <a href="#"
                     class="text-decoration-none z-index main-color border-r px-4 py-1 fw-bold border-0 text-light">View
                     all Video</a>
@@ -153,7 +153,7 @@
         </div>
         <div class="custom-container">
             <div class="d-flex justify-content-between align-items-center mb-3 mb-md-5">
-                <h1 class="fw-bold main-font-familly">Our Blogs</h1>
+                <h1 class="fw-bold heading-font">Our Blogs</h1>
                 <a href="#"
                     class="text-decoration-none z-index main-color border-r px-4 py-1 fw-bold border-0 text-light">View
                     all Markets</a>
@@ -165,15 +165,15 @@
                             <img src="img/img32.webp" class="p-3 border-r" alt="Card 1"
                                 class="card-img-top rounded" />
                             <div class="card-body text-start">
-                                <h5 class="card-title fw-bold body-font-familly f-22">
+                                <h5 class="card-title fw-bold  f-22">
                                     Sustainable Style: 4 Tips To Find Khadi Suit...
                                 </h5>
-                                <p class="card-text text-muted body-font-familly f-18">
+                                <p class="card-text text-muted  f-18">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                     Sapiente, inventore provident.
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text-muted body-font-familly">13 March 2024</small>
+                                    <small class="text-muted ">13 March 2024</small>
                                     <a href="#" class="text-decoration-none fw-semibold text-color">
                                         Read More
                                         <i class="fa-solid fa-arrow-right" style="color: #0e343d"></i>
@@ -186,15 +186,15 @@
                         <div class="card custom_card shadow-sm border-0 p-3">
                             <img src="img/img32.webp" alt="Card 1" class="card-img-top rounded" />
                             <div class="card-body text-start">
-                                <h5 class="card-title fw-bold body-font-familly f-22">
+                                <h5 class="card-title fw-bold  f-22">
                                     Sustainable Style: 4 Tips To Find Khadi Suit...
                                 </h5>
-                                <p class="card-text text-muted body-font-familly f-18">
+                                <p class="card-text text-muted  f-18">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                     Sapiente, inventore provident.
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text-muted body-font-familly">13 March 2024</small>
+                                    <small class="text-muted ">13 March 2024</small>
                                     <a href="#" class="text-decoration-none fw-semibold text-color">
                                         Read More
                                         <i class="fa-solid fa-arrow-right" style="color: #0e343d"></i>
@@ -207,15 +207,15 @@
                         <div class="card custom_card shadow-sm border-0 p-3">
                             <img src="img/img32.webp" alt="Card 1" class="card-img-top rounded" />
                             <div class="card-body text-start">
-                                <h5 class="card-title fw-bold body-font-familly f-22">
+                                <h5 class="card-title fw-bold  f-22">
                                     Sustainable Style: 4 Tips To Find Khadi Suit...
                                 </h5>
-                                <p class="card-text text-muted body-font-familly f-18">
+                                <p class="card-text text-muted  f-18">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                     Sapiente, inventore provident.
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text-muted body-font-familly">13 March 2024</small>
+                                    <small class="text-muted ">13 March 2024</small>
                                     <a href="#" class="text-decoration-none fw-semibold text-color">
                                         Read More
                                         <i class="fa-solid fa-arrow-right" style="color: #0e343d"></i>
@@ -228,15 +228,15 @@
                         <div class="card custom_card shadow-sm border-0 p-3">
                             <img src="img/img32.webp" alt="Card 1" class="card-img-top rounded" />
                             <div class="card-body text-start">
-                                <h5 class="card-title fw-bold body-font-familly f-22">
+                                <h5 class="card-title fw-bold  f-22">
                                     Sustainable Style: 4 Tips To Find Khadi Suit...
                                 </h5>
-                                <p class="card-text text-muted body-font-familly f-18">
+                                <p class="card-text text-muted  f-18">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                     Sapiente, inventore provident.
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text-muted body-font-familly">13 March 2024</small>
+                                    <small class="text-muted ">13 March 2024</small>
                                     <a href="#" class="text-decoration-none fw-semibold text-color">
                                         Read More
                                         <i class="fa-solid fa-arrow-right" style="color: #0e343d"></i>
@@ -249,15 +249,15 @@
                         <div class="card custom_card shadow-sm border-0 p-3">
                             <img src="img/img32.webp" alt="Card 1" class="card-img-top rounded" />
                             <div class="card-body text-start">
-                                <h5 class="card-title fw-bold body-font-familly f-22">
+                                <h5 class="card-title fw-bold  f-22">
                                     Sustainable Style: 4 Tips To Find Khadi Suit...
                                 </h5>
-                                <p class="card-text text-muted body-font-familly f-18">
+                                <p class="card-text text-muted  f-18">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                                     Sapiente, inventore provident.
                                 </p>
                                 <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text-muted body-font-familly">13 March 2024</small>
+                                    <small class="text-muted ">13 March 2024</small>
                                     <a href="#" class="text-decoration-none fw-semibold text-color">
                                         Read More
                                         <i class="fa-solid fa-arrow-right" style="color: #0e343d"></i>
