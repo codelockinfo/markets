788c7e4b6500ad5d42b3bf7298f97adb0d1183dc
urvashi <urvashi.codelock99@gmail.com>
2025-01-02 09:00:51 +0530

    invoice listing filter issue fixed on mobile

--- admin/invoice-list.php
+++ admin/invoice-list.php
@@ -22,17 +22,19 @@ if (!isset($_SESSION['current_user']['user_id'])) {
       <div class="card mb-4">
         <div class="d-flex justify-content-end p-3 align-items-center">
 
-          <div class="ms-md-auto d-flex align-items-center ">
-            <div class="me-3 invoice_fliter">
-            <input type="text" class="form-control search-btn_1 w-100 " id="date_range" name="date_range" placeholder="Select Date Range" />
-            </div>
+          <div class="ms-md-auto d-flex align-items-center filterheader">
 
-            <div class="input-group search-btn search-icon dropdownhide">
+            <div class="input-group search-btn search-icon dropdownhide w-100">
               <span class="input-group-text text-body search-btn_2"><i class="fas fa-search" aria-hidden="true"></i></span>
               <input type="text" class="form-control search-btn_1 me-2" placeholder="Type here..." id="search" data-routine="invoicelisting" >
             </div>
-            <div class="dropdown mt-3 filterDropdown"  data-filter="invoicelist">
-            <button class="btn bg-gradient-info dropdown-toggle dropdownhide " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+          
+            <div class="me-3 invoice_fliter w-50">
+            <input type="text" class="form-control search-btn_1 w-100 " id="date_range" name="date_range" placeholder="Select Date Range" />
+            </div>
+            
+            <div class="dropdown mt-3 filterDropdown w-40 text-end"  data-filter="invoicelist">
+            <button class="btn bg-gradient-info dropdown-toggle dropdownhide w-100" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
             Sort By
             </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" data-table="invoice">
