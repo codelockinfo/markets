ea6f513a50b4afffd45e969b70dd6bec33a500ad
isha <ishacodelock@gmail.com>
2025-01-03 11:15:45 +0530

    echo remove

--- admin/functions.php
+++ admin/functions.php
@@ -257,7 +257,7 @@ $limit = 12;
             } else {
                 if (move_uploaded_file($_FILES['shop_img']['tmp_name'], $fullpath) && move_uploaded_file($_FILES['shop_logo']['tmp_name'], $shopLogoPath)) {
                     $hashed_password = md5($password);
-                    echo $query = "INSERT INTO ". TABLE_USER ." (name, shop, address, phone_number, business_type, shop_logo, shop_img, password, email,verify_email_token) 
+                     $query = "INSERT INTO ". TABLE_USER ." (name, shop, address, phone_number, business_type, shop_logo, shop_img, password, email,verify_email_token) 
                               VALUES ('$name', '$shop', '$address', '$phone_number', '$business_type', '$shoplogo', '$newFilename', '$hashed_password', '$email','$verify_email_token')";
                     $result = mysqli_query($this->db, $query);
                     if ($result) {
